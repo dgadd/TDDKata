0f1369bd2d299d1a31e2b99d27305aa721dd5762
David Gadd <gaddzeit@yahoo.ca>
2010-11-14 11:48:15 -0800

    TDD-NOV14 First test in Calculator kata is added and passing

---
+++ TDDKata_Calculator_2010Nov14/Gaddzeit.Kata.Domain/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Domain
+{
+    public class Calculator
+    {
+        public int Add(string numbersToAdd)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TDDKata_Calculator_2010Nov14/Gaddzeit.Kata.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddMethod_ZeroLengthInput_Returns0()
+        {
+            var sut = new Calculator();
+            int result = sut.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
