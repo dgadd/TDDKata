ff617d84b89ed96a971afc0bf72aacbeb5e24f1b
David Gadd <gaddzeit@yahoo.ca>
2010-11-18 22:57:03 -0800

    TDD-MVC Starting creating fake repository tests; NOT all tests are passing yet"

---
+++ SportsStore/Tests/Integration/ProductsControllerIntegrationTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DomainModel.Abstract;
+using DomainModel.Entities;
+using DomainModel.Fakes;
+using NUnit.Framework;
+using WebUI.Controllers;
+
+namespace Tests.Integration
+{
+    [TestFixture]
+    public class ProductsControllerIntegrationTests
+    {
+        [Test]
+        public void dontknow()
+        {
+            IProductsRepository productsRepository = new FakeProductsRepository();
+            var products = new List<Product>
+                               {
+                                   new Product { ProductId = 35, Category = "computer"}
+                               };
+            IQueryable<Product> productsQuery = products.AsQueryable();
+
+            
+            var sut = new ProductController(productsRepository);
+            Assert.AreEqual(productsQuery, sut.GetProducts().ViewData.Model);            
+        }
+    }
+}
