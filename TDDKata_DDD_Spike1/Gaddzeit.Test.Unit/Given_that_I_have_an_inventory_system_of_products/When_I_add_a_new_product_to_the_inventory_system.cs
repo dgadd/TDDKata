9d9bcc400ce842804ed7542524ccdf507dc097ed
David Gadd <gaddzeit@yahoo.ca>
2011-10-22 11:13:37 -0700

    KATA-1 Final test

--- TDDKata_DDD_Spike1/Gaddzeit.Test.Unit/Given_that_I_have_an_inventory_system_of_products/When_I_add_a_new_product_to_the_inventory_system.cs
+++ TDDKata_DDD_Spike1/Gaddzeit.Test.Unit/Given_that_I_have_an_inventory_system_of_products/When_I_add_a_new_product_to_the_inventory_system.cs
@@ -24,5 +24,17 @@ namespace Gaddzeit.Test.Unit.Given_that_I_have_an_inventory_system_of_products
             });
             Assert.IsTrue(sut.Products.Any(p => p.Brand == brand && p.Model == model));
         }
+
+        [Test]
+        public void AddMethod_AnyLengthInput_ReturnsSum()
+        {
+            var expected = 0;
+            var numbersToAdd = "";
+            Enumerable.Range(2, new Random().Next(3, 999)).ToList().ForEach(x =>
+                                                             {
+                                                                 expected += x;
+                                                                 numbersToAdd += x + ",";
+                                                             });
+        }
     }
 }
