0970cdcc9ece4308925785bb8555b2bb2e835b7c
David Gadd <gaddzeit@yahoo.ca>
2011-10-22 11:46:45 -0700

    KATA-2 Test1: Two instances of DomainEntityBase are equal

---
+++ TDDKata_GymMembership_2011Oct22/Gaddzeit.Kata.Domain.Tests.Unit/DomainEntityBaseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Domain.Tests.Unit
+{
+    [TestFixture]
+    public class DomainEntityBaseTests
+    {
+        [Test]
+        public void TwoInstances_SameIdProperty_AreEqual()
+        {
+            const int id = 13515;
+            var sut1 = new DomainEntityBase { Id = id };
+            var sut2 = new DomainEntityBase { Id = id };
+
+            Assert.AreEqual(sut1, sut2);
+        }
+    }
+}
---
+++ TDDKata_GymMembership_2011Oct22/Gaddzeit.Kata.Domain/DomainEntityBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Domain
+{
+    public class DomainEntityBase
+    {
+        private int _id;
+
+        public int Id
+        {
+            get {
+                return _id;
+            }
+            set {
+                _id = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            DomainEntityBase other = obj as DomainEntityBase;
+            return other != null && other.Id.Equals(this.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id;
+        }
+    }
+}
