a6a609a9d281cd31c6dc44eecda265218a609ab3
David Gadd <gaddzeit@yahoo.ca>
2011-10-22 15:36:51 -0700

    KATA-2 Test 4: Gym is instance of DomainEntityBase

---
+++ TDDKata_GymMembership_2011Oct22/Gaddzeit.Kata.Domain.Tests.Unit/GymTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Domain.Tests.Unit
+{
+    [TestFixture]
+    public class GymTests
+    {
+        [Test]
+        public void Constructor_NoInput_IsInstanceOfDomainEntityBase()
+        {
+            var sut = new Gym();
+            Assert.IsInstanceOf(typeof(DomainEntityBase), sut);
+        }
+    }
+}
