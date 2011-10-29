0842b08c3bdb24a55d9a2b28a32d5a0eddc8b0f0
David Gadd <gaddzeit@yahoo.ca>
2011-10-29 11:15:33 -0700

    KATA-3 Tests 1 and 2: DomainEntityBase equality

---
+++ TDDKata_InvoicingSystem_2011Oct29/Gaddzeit.Kata.Domain/DomainEntityBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Domain
+{
+    public class DomainEntityBase
+    {
+        private int _id;
+
+        public int Id
+        {
+            get {
+                return _id;
+            }
+            set {
+                _id = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = (DomainEntityBase) obj;
+            return other.Id.Equals(this.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
--- TDDKata_InvoicingSystem_2011Oct29/Gaddzeit.Kata.Tests.Unit/DomainEntityBaseTests.cs
+++ TDDKata_InvoicingSystem_2011Oct29/Gaddzeit.Kata.Tests.Unit/DomainEntityBaseTests.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
 
 namespace Gaddzeit.Kata.Tests.Unit
 {
+    [TestFixture]
     public class DomainEntityBaseTests
     {
+        [Test]
+        // alternate name: Constructor_SameIdInputs_AreEqual
+        public void Two_instances_of_DomainEntityBase_are_equal_when_they_have_the_same_ID_values()
+        {
+            const int Id = 15253;
+            var sut1 = new DomainEntityBase { Id = Id };
+            var sut2 = new DomainEntityBase { Id = Id };
+
+            Assert.AreEqual(sut1, sut2);
+        }
+
+        [Test]
+        // alternate name: Constructor_DifferentIdInputs_AreNotEqual
+        public void Two_instances_of_DomainEntityBase_are_not_equal_when_they_have_different_ID_values()
+        {
+            var sut1 = new DomainEntityBase { Id = 123512 };
+            var sut2 = new DomainEntityBase { Id = 1123 };
+
+            Assert.AreNotEqual(sut1, sut2);
+        }
     }
 }
