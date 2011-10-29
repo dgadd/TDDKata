0b88356c169cad4dc6a02fc62b0f2868e4961703
David Gadd <gaddzeit@yahoo.ca>
2011-10-29 11:03:13 -0700

    KATA-3 Setup project

---
+++ TDDKata_InvoicingSystem_2011Oct29/Gaddzeit.Kata.Tests.Unit/DomainEntityBaseTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    public class DomainEntityBaseTests
+    {
+    }
+}
