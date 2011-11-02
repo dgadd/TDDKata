8f72ce193d95ce2c0fdeaa1093b751d35ec83a27
David Gadd <gaddzeit@yahoo.ca>
2011-11-01 19:48:29 -0700

    KATA-5 Test 5: Money created by parameterized constructor

---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Domain/Money.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Domain
+{
+    public struct Money
+    {
+        private readonly decimal _amount;
+        private readonly string _currency;
+
+        public Money(decimal amount, string currency)
+        {
+            _amount = amount;
+            _currency = currency;
+        }
+
+        public decimal Amount
+        {
+            get {
+                return _amount;
+            }
+        }
+
+        public string Currency
+        {
+            get {
+                return _currency;
+            }
+        }
+    }
+}
---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Tests.Unit/MoneyTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class MoneyTests
+    {
+        [Test]
+        public void Constructor_AmountAndCurrencyInputs_MatchGetterProperties()
+        {
+            const decimal amount = 3.25M;
+            const string currency = "CDN";
+            var sut = new Money(amount, currency);
+
+            Assert.AreEqual(amount, sut.Amount);
+            Assert.AreEqual(currency, sut.Currency);
+        }
+    }
+}
