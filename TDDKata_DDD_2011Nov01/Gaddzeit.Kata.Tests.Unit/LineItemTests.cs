a5598683603aced2843aba50d02b023d2dc2a53c
David Gadd <gaddzeit@yahoo.ca>
2011-11-01 19:36:15 -0700

    KATA-5 Test 4: LineItem instance of base class

---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Tests.Unit/LineItemTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class LineItemTests
+    {
+        [Test]
+        public void Constructor_NoINputs_IsInstanceOfDomainEntityBase()
+        {
+            var sut = new LineItem();
+            Assert.IsInstanceOf(typeof(DomainEntityBase), sut);
+        }
+    }
+}
