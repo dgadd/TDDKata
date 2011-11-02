b517a6effdc1488d157315104d1e2e6f32b4b7f5
David Gadd <gaddzeit@yahoo.ca>
2011-11-01 19:26:56 -0700

    KATA-5 Test 1: Base class with same Id are equal

---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Domain/DomainEntityBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Gaddzeit.Kata.Domain
+{
+    public class DomainEntityBase
+    {
+        private int _id;
+
+        public int Id
+        {
+            get {
+                return _id;
+            }
+            set {
+                _id = value;
+            }
+        }
+
+        public bool Equals(DomainEntityBase other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return other._id == _id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (DomainEntityBase)) return false;
+            return Equals((DomainEntityBase) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return _id;
+        }
+
+        public static bool operator ==(DomainEntityBase left, DomainEntityBase right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(DomainEntityBase left, DomainEntityBase right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Tests.Unit/DomainEntityBaseTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class DomainEntityBaseTests
+    {
+        [Test]
+        public void TwoInstance_SameIdInput_AreEqual()
+        {
+            const int id = 1325123;
+            var sut1 = new DomainEntityBase { Id = id };
+            var sut2 = new DomainEntityBase { Id = id };
+
+            Assert.AreEqual(sut1, sut2);
+        }
+    }
+}
