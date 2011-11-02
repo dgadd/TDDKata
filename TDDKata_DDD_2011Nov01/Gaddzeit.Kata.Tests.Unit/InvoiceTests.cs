45629e99a0cea60af6e2cf0de9e5bbb2957c326f
David Gadd <gaddzeit@yahoo.ca>
2011-11-01 19:34:24 -0700

    KATA-5 Test 4: Invoice is instance of base class

---
+++ TDDKata_DDD_2011Nov01/Gaddzeit.Kata.Tests.Unit/InvoiceTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class InvoiceTests
+    {
+        [Test]
+        public void Constructor_NoINputs_IsInstanceOfDomainEntityBase()
+        {
+            var sut = new Invoice();
+            Assert.IsInstanceOf(typeof(DomainEntityBase), sut);
+        }
+    }
+}
