96d5c790e3c20bf00b93f974a5e69059cf45c5a5
David Gadd <gaddzeit@yahoo.ca>
2011-11-24 19:48:02 -0800

    KATA-1 Added base class type tests for Inventory, Product, Item

---
+++ TDDKata_DDD_Part2/Gaddzeit.Kata.Tests.Unit/ItemTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class ItemTests
+    {
+        [Test]
+        public void Constructor_NoINputs_IsInstanceOfDomainEntityBase()
+        {
+            var sut = new Item();
+            Assert.IsInstanceOf(typeof(DomainEntityBase), sut);
+        }
+    }
+}
---
+++ TDDKata_DDD_Part2/Gaddzeit.Kata.Tests.Unit/ProductTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gaddzeit.Kata.Domain;
+using NUnit.Framework;
+
+namespace Gaddzeit.Kata.Tests.Unit
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void Constructor_NoINputs_IsInstanceOfDomainEntityBase()
+        {
+            var sut = new Product();
+            Assert.IsInstanceOf(typeof(DomainEntityBase), sut);
+        }
+    }
+}
