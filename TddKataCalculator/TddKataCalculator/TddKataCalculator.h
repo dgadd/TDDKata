987e21cd092d0b89e4d871e32c57a7b5fd162940
David Gadd <gaddzeit@yahoo.ca>
2011-12-01 21:00:08 -0800

    KATA-2 Project created

---
+++ TddKataCalculator/TddKataCalculator/TddKataCalculator.h
@@ -0,0 +1,13 @@
+//
+//  TddKataCalculator.h
+//  TddKataCalculator
+//
+//  Created by David Gadd on 12/01/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TddKataCalculator : SenTestCase
+
+@end
---
+++ TddKataCalculator/TddKataCalculator/TddKataCalculator.m
@@ -0,0 +1,32 @@
+//
+//  TddKataCalculator.m
+//  TddKataCalculator
+//
+//  Created by David Gadd on 12/01/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TddKataCalculator.h"
+
+@implementation TddKataCalculator
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TddKataCalculator");
+}
+
+@end
