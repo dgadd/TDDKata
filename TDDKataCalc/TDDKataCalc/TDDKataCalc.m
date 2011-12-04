4031e2c8c3de31fe0eb50620d43fab4fa63132b9
David Gadd <gaddzeit@yahoo.ca>
2011-12-03 18:09:07 -0800

    KATA-4 Project created. Builds and runs default test

---
+++ TDDKataCalc/TDDKataCalc/TDDKataCalc.m
@@ -0,0 +1,32 @@
+//
+//  TDDKataCalc.m
+//  TDDKataCalc
+//
+//  Created by David Gadd on 12/03/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TDDKataCalc.h"
+
+@implementation TDDKataCalc
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TDDKataCalc");
+}
+
+@end
