fbfd0e595d40d71c91d9c216ab10e68395fbde33
David Gadd <gaddzeit@yahoo.ca>
2011-12-07 21:04:31 -0800

    KATA-5 Test 2: One-length input returns equivalent

--- TddKataCalc/TddKataCalc/TddKataCalc.m
+++ TddKataCalc/TddKataCalc/TddKataCalc.m
@@ -26,7 +26,13 @@
 - (void)testAddMethod_ZeroLengthInput_ReturnsZero {
     int result = [sut add:@""];
     int expected = 0;
-    STAssertEqualObjects(expected, result, "Zero length input should return 0");
+    STAssertEquals(expected, result, "Zero length input should return 0");
+}
+
+- (void)testAddMethod_OneLengthInput_ReturnsEquivalent {
+    int result = [sut add:@"3"];
+    int expected = 3;
+    STAssertEquals(expected, result, "One length input should return equivalent");
 }
 
 @end
