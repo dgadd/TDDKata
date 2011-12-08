f82ea20968394d2f8a8dc7dd68d5d62a07e6694b
David Gadd <gaddzeit@yahoo.ca>
2011-12-07 21:17:40 -0800

    KATA-5 Test 3: Two-length string returns sum

--- TddKataCalc/TddKataCalc/Calculator.m
+++ TddKataCalc/TddKataCalc/Calculator.m
@@ -12,7 +12,21 @@
 
 }
 
+- (BOOL)containsWithin:(NSString *)thisString theFollowing:(NSString *)value {
+    return [thisString rangeOfString:value].location != NSNotFound;
+}
+
+- (int)sumOf:(NSString *)numbersToAdd {
+        int total = 0;
+        NSArray *numbersArray = [numbersToAdd componentsSeparatedByString:@","];
+        for(NSString *numberString in numbersArray)
+            total += [numberString intValue];
+        return total;
+    }
+
 - (int)add:(NSString *)numbersToAdd {
- return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
+    if ([self containsWithin:numbersToAdd theFollowing:@","])
+        return [self sumOf:numbersToAdd];
+    return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
