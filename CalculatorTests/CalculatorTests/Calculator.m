2c8dd7d5876bfb435db4e9ba005fa2ea786a7ec1
David Gadd <gaddzeit@yahoo.ca>
2011-12-10 10:18:30 -0800

    KATA-8 Test 4 Any length input returns sum

--- CalculatorTests/CalculatorTests/Calculator.m
+++ CalculatorTests/CalculatorTests/Calculator.m
@@ -9,15 +9,18 @@
     return [stringToSearch rangeOfString:value].location != NSNotFound;
 }
 
-- (int)add:(NSString *)numbersToAdd {
-    if ([self containsWithin:numbersToAdd theSubstring:@","])
-    {
+- (int)sum:(NSString *)numbersToAdd {
         int total = 0;
         NSArray *numbersArray = [numbersToAdd componentsSeparatedByString:@","];
         for(NSString *numberString in numbersArray)
             total += [numberString intValue];
         return total;
     }
+
+- (int)add:(NSString *)numbersToAdd {
+    if ([self containsWithin:numbersToAdd theSubstring:@","])
+        return [self sum:numbersToAdd];
+
     return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
