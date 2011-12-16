7de9b3e043a1811852e25e6ac71e4e0e551d43fe
David Gadd <gaddzeit@yahoo.ca>
2011-12-16 08:51:35 -0800

    KATA-11 Test 3 Two length string returns sum

--- CalculatorTests/CalculatorTests/Calculator.m
+++ CalculatorTests/CalculatorTests/Calculator.m
@@ -5,7 +5,21 @@
 
 }
 
+- (BOOL)contains:(NSString *)input theString:(NSString *)searchString {
+    return [input rangeOfString:searchString].location != NSNotFound;
+}
+
+- (int)sum:(NSString *)commaDelimitedString {
+        int total = 0;
+        NSArray *numbersArray = [commaDelimitedString componentsSeparatedByString:@","];
+        for(NSString *numberString in numbersArray)
+            total += [numberString intValue];
+        return total;
+    }
+
 - (int)add:(NSString *)numbersToAdd {
+    if ([self contains:numbersToAdd theString:@","])
+        return [self sum:numbersToAdd];
     return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
--- CalculatorTests/CalculatorTests/CalculatorTests.m
+++ CalculatorTests/CalculatorTests/CalculatorTests.m
@@ -27,6 +27,13 @@
     STAssertEquals(expected, result, @"One-length input should return equivalent");
 }
 
+- (void)testAddMethod_TwoLengthInput_ReturnsSum {
+    int result = [sut add:@"7,4"];
+    int expected = 11;
+
+    STAssertEquals(expected, result, @"Two-length input should return sum");
+}
+
 - (void)dealloc {
     [super dealloc];
 }
