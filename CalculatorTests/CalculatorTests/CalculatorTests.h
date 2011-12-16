232ac808fbf8c0387f8ae2ec320f166a96cc3a2e
David Gadd <gaddzeit@yahoo.ca>
2011-12-15 19:57:04 -0800

    KATA-10 Project created, builds, default test runs and fails

---
+++ CalculatorTests/CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by David Gadd on 12/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
---
+++ CalculatorTests/CalculatorTests/CalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  CalculatorTests.m
+//  CalculatorTests
+//
+//  Created by David Gadd on 12/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorTests.h"
+
+@implementation CalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CalculatorTests");
+}
+
+@end
