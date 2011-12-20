4952c3ad20c0b16bc4d48690497cab448501778e
David Gadd <gaddzeit@yahoo.ca>
2011-12-20 08:25:28 -0800

    SUBCLASS-1 Created Person.h interface

--- PersonTests/PersonTests/Person.h
+++ PersonTests/PersonTests/Person.h
@@ -1,5 +1,23 @@
 #import <Foundation/Foundation.h>
 
+typedef enum {
+    Male, Female
+} Gender;
 
-@interface Person : NSObject
+@interface Person : NSObject {
+    NSString *name;
+    NSNumber *age;
+    Gender gender;
+}
+
+- (id)initWithName:(NSString *)_name;
+
+- (id)initWithAge:(NSNumber *)_age;
+
+- (id)initWithGender:(Gender)_gender;
+
+- (id)initWithName:(NSString *)_name
+               age:(NSNumber *)_age
+            gender:(Gender)_gender;
+
 @end
