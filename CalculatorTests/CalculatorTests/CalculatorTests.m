af18fb2ac321ff580f7f9574852f30bfefe57e66
David Gadd <gaddzeit@yahoo.ca>
2011-12-26 14:46:16 -0800

    KATA-17 Test 1 Zero length input returns 0.

--- CalculatorTests/CalculatorTests/CalculatorTests.m
+++ CalculatorTests/CalculatorTests/CalculatorTests.m
@@ -1,32 +1,28 @@
-//
-//  CalculatorTests.m
-//  CalculatorTests
-//
-//  Created by David Gadd on 12/26/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import "CalculatorTests.h"
+#import "Calculator.h"
 
 @implementation CalculatorTests
 
-- (void)setUp
-{
+- (void)setUp {
     [super setUp];
-    
-    // Set-up code here.
+    sut = [[Calculator alloc] init];
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
+- (void)tearDown {
+    [sut release];
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in CalculatorTests");
+- (void)testAddMethod_ZeroLengthInput_ReturnsZero {
+    int result = [sut add:@""];
+    int expected = 0;
+
+    STAssertEquals(expected, result, @"Zero-length input returns zero.");
 }
+
+- (void)dealloc {
+    [super dealloc];
+}
+
 
 @end
