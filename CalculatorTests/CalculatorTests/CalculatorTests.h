dc04fbabec0d0d3fc9d5defd01119a7b7584eb30
David Gadd <gaddzeit@yahoo.ca>
2011-12-26 14:42:33 -0800

    KATA-17 Created project

---
+++ CalculatorTests/CalculatorTests/CalculatorTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorTests.h
+//  CalculatorTests
+//
+//  Created by David Gadd on 12/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorTests : SenTestCase
+
+@end
