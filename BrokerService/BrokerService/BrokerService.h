c50b6d0e7b0049a2563b05091a651a4708a839d9
David Gadd <gaddzeit@yahoo.ca>
2012-01-01 22:59:14 -0800

    OCMOCK-1 Project is created and building

---
+++ BrokerService/BrokerService/BrokerService.h
@@ -0,0 +1,13 @@
+//
+//  BrokerService.h
+//  BrokerService
+//
+//  Created by David Gadd on 01/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BrokerService : SenTestCase
+
+@end
---
+++ BrokerService/BrokerService/BrokerService.m
@@ -0,0 +1,32 @@
+//
+//  BrokerService.m
+//  BrokerService
+//
+//  Created by David Gadd on 01/01/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BrokerService.h"
+
+@implementation BrokerService
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BrokerService");
+}
+
+@end
