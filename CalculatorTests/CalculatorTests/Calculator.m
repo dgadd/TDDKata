1054d1b722005506f4ef75fa088d7ca5d5534937
David Gadd <gaddzeit@yahoo.ca>
2012-01-04 07:40:42 -0800

    KATA-20 Test 3 Two length input returns sum

--- CalculatorTests/CalculatorTests/Calculator.m
+++ CalculatorTests/CalculatorTests/Calculator.m
@@ -5,7 +5,21 @@
 
 }
 
+- (BOOL)containsWithin:(NSString *)thisString theValue:(NSString *)thisValue {
+    return [thisString rangeOfString:thisValue].location != NSNotFound;
+}
+
+- (int)sum:(NSString *)numbersToAdd {
+    NSArray *numbersArray = [numbersToAdd componentsSeparatedByString:@","];
+    int total = 0;
+    for (NSString *numberString in numbersArray)
+        total += [numberString intValue];
+    return total;
+}
+
 - (int)add:(NSString *)numbersToAdd {
+    if ([self containsWithin:numbersToAdd theValue:@","])
+        return [self sum:numbersToAdd];
     return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
