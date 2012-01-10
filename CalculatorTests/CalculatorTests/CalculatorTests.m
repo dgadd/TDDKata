3891e8c7e783e6e45c584dd9e5bf0d16f1ddbd92
David Gadd <gaddzeit@yahoo.ca>
2012-01-10 06:38:35 -0800

    KATA-25 Created project

---
+++ CalculatorTests/CalculatorTests/CalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  CalculatorTests.m
+//  CalculatorTests
+//
+//  Created by David Gadd on 01/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorTests.h"
+
+@implementation CalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CalculatorTests");
+}
+
+@end
