93eeec16b95d09a20daea5a23c9290eeba8f5ffc
David Gadd <gaddzeit@yahoo.ca>
2012-01-12 06:56:18 -0800

    KATA-26 Test 1 Zero length input returns 0

--- CalculatorTests/CalculatorTests/CalculatorTests.h
+++ CalculatorTests/CalculatorTests/CalculatorTests.h
@@ -1,13 +1,9 @@
-//
-//  CalculatorTests.h
-//  CalculatorTests
-//
-//  Created by David Gadd on 01/12/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <SenTestingKit/SenTestingKit.h>
 
-@interface CalculatorTests : SenTestCase
+@class Calculator;
+
+@interface CalculatorTests : SenTestCase {
+    Calculator *sut;
+}
 
 @end
