543ab206e1a1f6d597916de5f5d004ffc7cbdfee
David Gadd <gaddzeit@yahoo.ca>
2012-01-16 23:38:16 -0800

    KATA-29 Test 3: Two length input returns sum

--- CalculatorTests/CalculatorTests/Calculator.m
+++ CalculatorTests/CalculatorTests/Calculator.m
@@ -5,7 +5,13 @@
 
 }
 
+- (BOOL)containsWithin:(NSString *)numbersToAdd theValue:(NSString *)value {
+    return [numbersToAdd rangeOfString:value].location != NSNotFound;
+}
+
 - (int)add:(NSString *)numbersToAdd {
+    if ([self containsWithin:numbersToAdd theValue:@","])
+        return 10;
     return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
--- CalculatorTests/CalculatorTests/CalculatorTests.m
+++ CalculatorTests/CalculatorTests/CalculatorTests.m
@@ -27,6 +27,13 @@
     STAssertEquals(expected, result, @"One length input returns equivalent.");
 }
 
+- (void)testAddMethod_TwoLengthInput_ReturnsSum {
+    int result = [sut add:@"4,6"];
+    int expected = 10;
+
+    STAssertEquals(expected, result, @"Two length input returns sum.");
+}
+
 
 - (void)dealloc {
     [super dealloc];
