3d678c461607915104a7f431afac5584b34e598b
David Gadd <gaddzeit@yahoo.ca>
2012-01-22 11:35:40 -0800

    KATA-31 Test 2: One length input should return equivalent

--- CalcualtorTests/CalcualtorTests/CalcualtorTests.m
+++ CalcualtorTests/CalcualtorTests/CalcualtorTests.m
@@ -14,10 +14,17 @@
 }
 
 - (void)testAddMethod_ZeroLengthInput_ReturnsZero {
-    int result = [sut add:@","];
+    int result = [sut add:@""];
     int expected = 0;
 
     STAssertEquals(expected, result, @"Zero length input should return 0.");
+}
+
+- (void)testAddMethod_OneLengthInput_ReturnsEquivalent {
+    int result = [sut add:@"3"];
+    int expected = 3;
+
+    STAssertEquals(expected, result, @"One length input should return equivalent.");
 }
 
 
