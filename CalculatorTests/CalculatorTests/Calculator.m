d6893a67fdfc2abd14983c9a07bd2e48bbe5507f
David Gadd <gaddzeit@yahoo.ca>
2012-01-28 07:08:09 -0800

    KATA-32 Test 3: Two length input should return sum.

--- CalculatorTests/CalculatorTests/Calculator.m
+++ CalculatorTests/CalculatorTests/Calculator.m
@@ -6,6 +6,14 @@
 }
 
 - (int)add:(NSString *)numbersToAdd {
+    if ([numbersToAdd rangeOfString:@","].location != NSNotFound) {
+        int total = 0;
+        NSArray *numbersArray = [numbersToAdd componentsSeparatedByString:@","];
+        for(NSString *numberString in numbersArray) {
+            total += [numberString intValue];
+        }
+        return total;
+    }
     return [numbersToAdd length] > 0 ? [numbersToAdd intValue] : 0;
 }
 @end
