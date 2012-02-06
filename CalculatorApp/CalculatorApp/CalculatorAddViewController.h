840518380470a04ae01b577e4e606c11ee1ccadf
David Gadd <gaddzeit@yahoo.ca>
2012-02-06 06:56:05 -0800

    KATA-1 Created empty app with custom view controller class and new storyboard

---
+++ CalculatorApp/CalculatorApp/CalculatorAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorAddViewController.h
+//  CalculatorApp
+//
+//  Created by David Gadd on 12-02-06.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAddViewController : UIViewController
+
+@end
---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorAppTests.h
+//  CalculatorAppTests
+//
+//  Created by David Gadd on 12-02-06.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorAppTests : SenTestCase
+
+@end
