62412aadf5a1e91873811ef4e90cdaaf25595ec5
David Gadd <gaddzeit@yahoo.ca>
2012-02-12 14:53:33 -0800

    KATA-3 Test 1: AtomicTransactionManager class is instnace of interface

---
+++ TDDKata_DDD_Part3/Kata.Repository.Tests.Unit/AtomicTransactionManagerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Kata.Repository.Tests.Unit
+{
+    [TestFixture]
+    public class AtomicTransactionManagerTests
+    {
+        [Test]
+        public void Constructor_NoInputs_IsInstanceOfInterface()
+        {
+            var sut = new AtomicTransactionManager();
+            Assert.IsInstanceOf(typeof(IAtomicTransactionManager), sut);
+        }
+    }
+}
