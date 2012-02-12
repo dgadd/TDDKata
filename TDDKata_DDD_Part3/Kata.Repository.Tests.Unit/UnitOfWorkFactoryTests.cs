24d7399477f6e1878271cbaab3282d95fc9276ce
David Gadd <gaddzeit@yahoo.ca>
2012-02-12 15:24:42 -0800

    KATA-3 UnitOfWorkFactory is instance of interface

---
+++ TDDKata_DDD_Part3/Kata.Repository.Tests.Unit/UnitOfWorkFactoryTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Kata.Repository.Tests.Unit
+{
+    [TestFixture]
+    public class UnitOfWorkFactoryTests
+    {
+        [Test]
+        public void Constructor_NoInputs_IsInstanceOfInterface()
+        {
+            var sut = new UnitOfWorkFactory();
+            Assert.IsInstanceOf(typeof(IUnitOfWorkFactory), sut);
+        }
+
+    }
+}
