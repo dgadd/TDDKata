8c42fda3f2bfd3886906aae77483286ba29a4c2f
David Gadd <gaddzeit@yahoo.ca>
2012-02-20 06:47:51 -0800

    KATA-36 Test 4: Any length input returns sum

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -12,21 +12,36 @@
     int result = [sut add:@""];
     int expected = 0;
 
-    STAssertEquals(expected, result, @"Zero lenght input should return 0.");
+    STAssertEquals(expected, result, @"Zero length input should return 0.");
 }
 
 - (void)testAddMethod_OneLengthInput_ReturnsEquivalent {
     int result = [sut add:@"3"];
     int expected = 3;
 
-    STAssertEquals(expected, result, @"One lenght input should return equivalent.");
+    STAssertEquals(expected, result, @"One length input should return equivalent.");
 }
 
 - (void)testAddMethod_TwoLengthInput_ReturnsSum {
     int result = [sut add:@"3,5"];
     int expected = 8;
 
-    STAssertEquals(expected, result, @"Two lenght input should return sum.");
+    STAssertEquals(expected, result, @"Two length input should return sum.");
+}
+
+- (void)testAddMethod_AnyLengthInput_ReturnsSum {
+    int howMany = arc4random() % 999;
+    int expected = 0;
+    NSMutableString *numbersToAdd = [NSMutableString string];
+
+    for(int i = 0; i < howMany; i++) {
+        [numbersToAdd appendString:[NSString stringWithFormat:@"%d,",i]];
+        expected += i;
+    }
+
+    int result = [sut add:numbersToAdd];
+
+    STAssertEquals(expected, result, @"Any length input should return sum.");
 }
 
 
