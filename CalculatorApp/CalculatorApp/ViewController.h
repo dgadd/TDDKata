6539bda74587fe46307200381fe2a7077fe71148
David Gadd <gaddzeit@yahoo.ca>
2012-02-25 21:00:28 -0800

    KATA-37 Added controls and IBOutlet/IBAction connections to ViewController

--- CalculatorApp/CalculatorApp/ViewController.h
+++ CalculatorApp/CalculatorApp/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *inputField;
 
+@property (strong, nonatomic) IBOutlet UITextField *resultField;
+- (IBAction)calculateButtonClicked:(id)sender;
 @end
