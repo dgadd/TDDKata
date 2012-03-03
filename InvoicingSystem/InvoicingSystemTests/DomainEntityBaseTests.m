d41c282bc4fe2378a593fbd435c7e69c58d292ab
David Gadd <gaddzeit@yahoo.ca>
2012-03-02 18:33:07 -0800

    DOMAINKATA-1 Two instances with different Id are not equal

--- InvoicingSystem/InvoicingSystemTests/DomainEntityBaseTests.m
+++ InvoicingSystem/InvoicingSystemTests/DomainEntityBaseTests.m
@@ -16,5 +16,13 @@
     STAssertEqualObjects(sut, sut2, @"sut should be equal to sut2");
 }
 
+- (void)testTwoInstances_DifferentIdInput_AreNotEqual {
+    DomainEntityBase *sut2 = [[DomainEntityBase alloc] init];
+    sut.Id = 352;
+    sut2.Id = 9999;
+
+    STAssertFalse([sut isEqual:sut2], @"sut should not equal sut2");
+}
+
 
 @end
