cfbc7221f01885f46c96dbcf1a767b2e0d49ce04
David Gadd <gaddzeit@yahoo.ca>
2012-03-31 23:34:32 -0700

    KATA-40 Test 3: Two length input returns sum.

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -23,5 +23,12 @@
     STAssertEquals(expected, result, @"One length input should return equivalent.");
 }
 
+- (void)testAddMethod_TwoLengthInput_ReturnsSum {
+    int result = [sut add:@"3,5"];
+    int expected = 8;
+
+    STAssertEquals(expected, result, @"Two length input should return sum.");
+}
+
 
 @end
