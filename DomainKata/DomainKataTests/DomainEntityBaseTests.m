6ff3d180afdd4e136f1c7520919f2e890a5c5965
David Gadd <gaddzeit@yahoo.ca>
2012-06-12 18:39:48 -0700

    DOMAIN-2012JUN12 Test 2: Two entities with different key are not equal

--- DomainKata/DomainKataTests/DomainEntityBaseTests.m
+++ DomainKata/DomainKataTests/DomainEntityBaseTests.m
@@ -15,4 +15,11 @@
     STAssertEqualObjects(sut1, sut2, @"Two intances of entity with same key should be equal");
 }
 
+- (void)testInitWithId_twoInstancesDifferentId_areNotEqual {
+    DomainEntityBase *sut1 = [[DomainEntityBase alloc] initWithKey:32523];
+    DomainEntityBase *sut2 = [[DomainEntityBase alloc] initWithKey:12351];
+
+    STAssertFalse([sut1 isEqual:sut2], @"Two intances of entity with same key should not be equal");
+}
+
 @end
