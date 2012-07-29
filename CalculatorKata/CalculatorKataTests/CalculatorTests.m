4de2b8a82b5e69579e5f47f2b50f930930b3e13f
David Gadd <gaddzeit@yahoo.ca>
2012-07-28 17:09:28 -0700

    KATA-JUL28 Test 2: One length input returns equivalent

--- CalculatorKata/CalculatorKataTests/CalculatorTests.m
+++ CalculatorKata/CalculatorKataTests/CalculatorTests.m
@@ -15,5 +15,12 @@
     STAssertEquals(expected, result, @"Zero length input should return 0.");
 }
 
+- (void)testAddMethod_oneLengthInput_shouldReturnEquivalent {
+    int expected = 3;
+    int result = [sut add:@"3"];
+
+    STAssertEquals(expected, result, @"One length input should return equivalent.");
+}
+
 
 @end
