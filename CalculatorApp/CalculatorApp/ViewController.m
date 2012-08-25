9d6f4139a742c91a1bc947720f2d78b41055f89f
David Gadd <gaddzeit@yahoo.ca>
2012-08-25 13:04:38 -0700

    Test 1: Zero length input returns 0

---
+++ CalculatorApp/CalculatorApp/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  CalculatorApp
+//
+//  Created by David Gadd on 08/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
