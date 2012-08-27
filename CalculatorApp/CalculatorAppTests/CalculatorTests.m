052b613ecc9ac094202eb658018bf4ffa4b55a87
David Gadd <gaddzeit@yahoo.ca>
2012-08-27 07:15:57 -0700

    Test 5: New line delimiter input should return sum

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -44,5 +44,12 @@
     STAssertEquals(expected, result, @"Two length input should return sum.");
 }
 
+- (void)testAddMethod_newLineDelimiterInput_returnsSum {
+    NSInteger expected = 12;
+    NSInteger result = [sut add:@"5,3\n4"];
+
+    STAssertEquals(expected, result, @"New line delimiter input should return sum.");
+}
+
 
 @end
