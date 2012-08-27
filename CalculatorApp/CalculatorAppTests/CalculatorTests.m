b8320db4c1f660bd79788ca4cfdc080ad1c9389b
David Gadd <gaddzeit@yahoo.ca>
2012-08-27 07:23:11 -0700

    Test 7: Custom delimiter input should return sum

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -61,5 +61,12 @@
     }
 }
 
+- (void)testAddMethod_customDelimiterInput_returnsSum {
+    NSInteger expected = 15;
+    NSInteger result = [sut add:@"//@\n3@5,3\n4"];
+
+    STAssertEquals(expected, result, @"Custom delimiter input should return sum.");
+}
+
 
 @end
