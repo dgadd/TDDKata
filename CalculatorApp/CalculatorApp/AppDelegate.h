e456aead4ba54b527f41c1c78fa9261c869e25f7
David Gadd <gaddzeit@yahoo.ca>
2012-08-27 07:08:10 -0700

    Test 1: Zero length input should return 0

---
+++ CalculatorApp/CalculatorApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CalculatorApp
+//
+//  Created by David Gadd on 08/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
