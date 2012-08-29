7d1577bf4429f3818b1a86f27a64f6124afd439e
David Gadd <gaddzeit@yahoo.ca>
2012-08-29 07:08:19 -0700

    Test 2: One length input should return equivalent

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -16,4 +16,11 @@
     STAssertEquals(expected, result, @"Zero length input should return 0.");
 }
 
+- (void)testAddMethod_oneLengthInput_returnsEquivalent {
+    NSInteger expected = 8;
+    NSInteger result = [sut add:@"8"];
+
+    STAssertEquals(expected, result, @"One length input should return equivalent.");
+}
+
 @end
