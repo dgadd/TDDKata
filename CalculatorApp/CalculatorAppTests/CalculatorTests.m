9697daaff27c3a453d8a22570c2dcfb313a0341b
David Gadd <gaddzeit@yahoo.ca>
2012-09-06 18:09:51 -0400

    Test 1: Zero length input should return 0

---
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -0,0 +1,17 @@
+#import "CalculatorTests.h"
+#import "Calculator.h"
+
+@implementation CalculatorTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)testAddMethod_zeroLengthInput_returnsZero {
+    NSInteger expected = 0;
+    NSInteger result = [sut add:@""];
+
+    STAssertEquals(expected, result, @"Zero length input should return 0.");
+}
+
+@end
