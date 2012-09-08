737c3d60d687d1c381f86a63d1e2cced6cd2d424
David Gadd <gaddzeit@yahoo.ca>
2012-09-08 11:11:23 -0400

    Test 2: Column width > 19 returns result with exact line wrap at width

--- WordWrapApp/WordWrapApp/Utils/StringManipulator.m
+++ WordWrapApp/WordWrapApp/Utils/StringManipulator.m
@@ -6,6 +6,19 @@
 }
 
 - (NSString *)lineWrap:(NSString *)input byColumnWidth:(int)width {
-    return input;
+    if (width == 0)
+        return input;
+
+    if (width > 19)
+    {
+        NSMutableString *inputWithLineBreaks = [NSMutableString string];
+        while ([input length] > width) {
+            NSString *segment = [input substringToIndex:width];
+            [inputWithLineBreaks appendString:[NSString stringWithFormat:@"%@\n", segment]];
+            input = [input stringByReplacingOccurrencesOfString:segment withString:@""];
+        }
+        [inputWithLineBreaks appendString:[NSString stringWithFormat:@"%@\n", input]];
+        return inputWithLineBreaks;
+    }
 }
 @end
--- WordWrapApp/WordWrapAppTests/StringManipulatorTests.m
+++ WordWrapApp/WordWrapAppTests/StringManipulatorTests.m
@@ -14,7 +14,16 @@
 
     NSString *result = [sut lineWrap:input byColumnWidth:0];
 
-    STAssertEqualObjects(result, expected, @"When column width is 0, result should have no line breaks.");
+    STAssertEqualObjects(expected, result, @"When column width is 0, result should have no line breaks.");
 }
 
+
+- (void)testLineWrapByColumnWidthMethod_columnWidthGreaterThan19_stringHasExactWidthLineBreaks {
+    NSString *input = @"Here is my string with no breaks, but enough length for at least 2 or 3 of them. Sound good?";
+    NSString *expected = @"Here is my string wi\nth no breaks, but en\nough length for at l\neast 2 or 3 of them.\n Sound good?\n";
+
+    NSString *result = [sut lineWrap:input byColumnWidth:20];
+
+    STAssertEqualObjects(expected, result, @"When column width > 19, result should have exact width line breaks.");
+}
 @end
