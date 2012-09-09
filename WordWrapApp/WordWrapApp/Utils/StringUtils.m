2cc669d8938062e844f06cffb21f917b04069f9a
David Gadd <gaddzeit@yahoo.ca>
2012-09-09 17:56:48 -0400

    Test 1: When colum width is 20, result should have exact line breaks

--- WordWrapApp/WordWrapApp/Utils/StringUtils.m
+++ WordWrapApp/WordWrapApp/Utils/StringUtils.m
@@ -6,6 +6,17 @@
 }
 
 - (NSString *)wrapLine:(NSString *)input byColumnWidth:(int)width {
-    return input;
+    if(width == 0)
+        return input;
+
+    NSMutableString *inputWithLineBreaks = [NSMutableString string];
+    while ([input length] > width) {
+        NSString *segment = [input substringToIndex:width];
+        input = [input stringByReplacingOccurrencesOfString:segment withString:@""];
+        [inputWithLineBreaks appendString:[NSString stringWithFormat:@"%@\n", segment]];
+    }
+    [inputWithLineBreaks appendString:[NSString stringWithFormat:@"%@\n", input]];
+
+    return inputWithLineBreaks;
 }
 @end
--- WordWrapApp/WordWrapAppTests/StringUtilsTests.m
+++ WordWrapApp/WordWrapAppTests/StringUtilsTests.m
@@ -17,4 +17,13 @@
     STAssertEqualObjects(expected, result, @"When column width is 0, result should have no line breaks.");
 }
 
+- (void)testWrapLineByColumnWidthMethod_inputAndColumnWidthTwenty_returnHasNoExactBreaks {
+    NSString *input = @"Here is some input that is greater than 20 character long, with almost-maybe some hyphens.";
+    NSString *expected = @"Here is some input t\nhat is greater than \n20 character long, w\nith almost-maybe som\ne hyphens.\n";
+
+    NSString *result = [sut wrapLine:input byColumnWidth:20];
+
+    STAssertEqualObjects(expected, result, @"When column width is 20, result should have exact line breaks.");
+}
+
 @end
