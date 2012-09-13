7a043f82a2e6a217a8ef87a1e4f16e539fef36c3
David Gadd <gaddzeit@yahoo.ca>
2012-09-13 19:03:34 -0400

    Test 1: Zero length input should return 0

---
+++ CalculatorApp/CalculatorApp/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  CalculatorApp
+//
+//  Created by David Gadd on 09/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
