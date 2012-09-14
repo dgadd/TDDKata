fee569ff2f543c0001b60601c9a0c1c0293502e9
David Gadd <gaddzeit@yahoo.ca>
2012-09-14 12:36:01 -0400

    Test 2: 20 length column width should return line with exact breaks

--- WordWrapApp/WordWrapApp/Utils/StringUtils.m
+++ WordWrapApp/WordWrapApp/Utils/StringUtils.m
@@ -6,6 +6,17 @@
 }
 
 - (NSString *)wrapLine:(NSString *)input byColumnWidth:(int)width {
-    return input;
+    if(width == 0)
+        return input;
+
+    NSMutableString *lineWithBreaks = [NSMutableString string];
+    while ([input length] > width) {
+        NSString *sequence = [input substringToIndex:width];
+        input = [input stringByReplacingOccurrencesOfString:sequence withString:@""];
+        [lineWithBreaks appendString:[NSString stringWithFormat:@"%@\n", sequence]];
+    }
+    [lineWithBreaks appendString:input];
+
+    return lineWithBreaks;
 }
 @end
--- WordWrapApp/WordWrapAppTests/StringUtilsTests.m
+++ WordWrapApp/WordWrapAppTests/StringUtilsTests.m
@@ -17,4 +17,13 @@
     STAssertEqualObjects(expected, result, @"0 length column width should return unwrapped line.");
 }
 
+- (void)testWrapLineByColumnWidthMethod_columnWidth20_shouldReturnExactLineBreaks {
+    NSString *input = @"Many multi-marmotted maps may multiply 3.14527 x more magnificently.";
+    NSString *expected = @"Many multi-marmotted\n maps may multiply 3\n.14527 x more magnif\nicently.";
+
+    NSString *result = [sut wrapLine:input byColumnWidth:20];
+
+    STAssertEqualObjects(expected, result, @"20 length column width should return exact line breaks.");
+}
+
 @end
