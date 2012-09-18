e1b9cd44d1d78af881784ceef0b455e6bb22aacb
David Gadd <gaddzeit@yahoo.ca>
2012-09-18 10:44:38 -0400

    Test 2: Column width 20 returns line with exact breaks

--- WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
+++ WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
@@ -17,4 +17,13 @@
     STAssertEqualObjects(expected, result, @"Column width 0 should return line with no breaks.");
 }
 
+- (void)testWrapLineByColumnWidthMethod_columnWidth20_returnsLineWithExactBreaks {
+    NSString *input = @"This is a complex, half-hyphenated line of text with numbers 2,138,454 and 'single quotes'.";
+    NSString *expected = @"This is a complex, h\nalf-hyphenated line \nof text with numbers\n 2,138,454 and 'sing\nle quotes'.";
+
+    NSString *result = [sut wrapLine:input byColumnWidth:20];
+
+    STAssertEqualObjects(expected, result, @"Column width 20 should return line with exact breaks.");
+}
+
 @end
