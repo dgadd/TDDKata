ccb68c1639c7650a45340cb7d45527c2270f0e28
David Gadd <gaddzeit@yahoo.ca>
2012-09-18 10:59:36 -0400

    Test 5 Word break true param breaks line at word boundaries within column width

--- WordWrapApp/WordWrapApp/Utils/StringUtils.m
+++ WordWrapApp/WordWrapApp/Utils/StringUtils.m
@@ -5,19 +5,32 @@
 
 }
 
-- (NSString *)wrapLine:(NSString *)input byColumnWidth:(int)width {
+- (NSString *)wrapLine:(NSString *)input byColumnWidth:(int)width withWordBreak:(BOOL)wordBreak {
     if(width == 0)
         return input;
-    if(width < 20)
-        [NSException raise:@"ColumnWidthTooNarrowException" format:@"You cannot input a column width < 20 (unless 0)."];
+    [self guardCondition_rejectNarrowColumnWidthFor:width];
 
     NSMutableString *lineWithBreaks = [NSMutableString string];
     while ([input length] > width) {
         NSString *sequence = [input substringToIndex:width];
+        if(wordBreak)
+            sequence = [self handleWordBreaksIn:sequence];
         input = [input stringByReplacingOccurrencesOfString:sequence withString:@""];
         [lineWithBreaks appendString:[NSString stringWithFormat:@"%@\n",sequence]];
     }
     [lineWithBreaks appendString:[NSString stringWithFormat:@"%@",input]];
     return lineWithBreaks;
+}
+
+- (void)guardCondition_rejectNarrowColumnWidthFor:(int)width {
+    if(width < 20)
+        [NSException raise:@"ColumnWidthTooNarrowException" format:@"You cannot input a column width < 20 (unless 0)."];
+}
+
+- (NSString *)handleWordBreaksIn:(NSString *)sequence {
+    NSUInteger lastSpaceInSequence = [sequence rangeOfString:@" " options:NSBackwardsSearch].location;
+    if (lastSpaceInSequence != NSNotFound)
+                sequence = [sequence substringToIndex:lastSpaceInSequence + 1];
+    return sequence;
 }
 @end
--- WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
+++ WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
@@ -12,7 +12,7 @@
     NSString *input = @"This is a complex, half-hyphenated line of text with numbers 2,138,454 and 'single quotes'.";
     NSString *expected = input;
 
-    NSString *result = [sut wrapLine:input byColumnWidth:0];
+    NSString *result = [sut wrapLine:input byColumnWidth:0 withWordBreak:NO];
 
     STAssertEqualObjects(expected, result, @"Column width 0 should return line with no breaks.");
 }
@@ -21,7 +21,7 @@
     NSString *input = @"This is a complex, half-hyphenated line of text with numbers 2,138,454 and 'single quotes'.";
     NSString *expected = @"This is a complex, h\nalf-hyphenated line \nof text with numbers\n 2,138,454 and 'sing\nle quotes'.";
 
-    NSString *result = [sut wrapLine:input byColumnWidth:20];
+    NSString *result = [sut wrapLine:input byColumnWidth:20 withWordBreak:NO];
 
     STAssertEqualObjects(expected, result, @"Column width 20 should return line with exact breaks.");
 }
@@ -30,18 +30,28 @@
     NSString *input = @"Short input";
     NSString *expected = input;
 
-    NSString *result = [sut wrapLine:input byColumnWidth:20];
+    NSString *result = [sut wrapLine:input byColumnWidth:20 withWordBreak:NO];
 
     STAssertEqualObjects(expected, result, @"Input text shorter than column width should return line with no breaks.");
 }
 
 - (void)testWrapLineByColumnWidthMethod_columnWidthShorterThanTwenty_throwsException {
     @try {
-        [sut wrapLine:@"blah" byColumnWidth:19];
+        [sut wrapLine:@"blah" byColumnWidth:19 withWordBreak:NO];
         STAssertFalse(true, @"Column width less than 20 should throw exception.");
     } @catch(NSException *ex) {
         STAssertEqualObjects(@"ColumnWidthTooNarrowException", [ex name], @"The expected exception name was not thrown.");
         STAssertEqualObjects(@"You cannot input a column width < 20 (unless 0).", [ex description], @"The expected exception description was not thrown.");
     }
 }
+
+- (void)testWrapLineByColumnWidthWithWordBreakMethod_columnWidth20_returnsLineWithWordBreaks {
+    NSString *input = @"This is a complex, half-hyphenated line of text with numbers 2,138,454 and 'single quotes'.";
+    NSString *expected = @"This is a complex, \nhalf-hyphenated \nline of text with \nnumbers 2,138,454 \nand 'single quotes'.";
+
+    NSString *result = [sut wrapLine:input byColumnWidth:20 withWordBreak:YES];
+
+    STAssertEqualObjects(expected, result, @"When wordBreak is YES, the line should break at word boundaries.");
+}
+
 @end
