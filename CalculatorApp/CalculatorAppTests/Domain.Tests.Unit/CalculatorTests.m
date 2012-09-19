e7b5284ef58d9a79bdfde5716954c1204b4dfbf8
David Gadd <gaddzeit@yahoo.ca>
2012-09-19 10:09:26 -0400

    Test 4: Any length input should return sum

--- CalculatorApp/CalculatorAppTests/Domain.Tests.Unit/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/Domain.Tests.Unit/CalculatorTests.m
@@ -29,5 +29,19 @@
     STAssertEquals(expected, result, @"Two length input should return sum.");
 }
 
+- (void)testAddMethod_anyLengthInput_returnsSum {
+    NSInteger randomAmount = arc4random() % 999;
+    NSInteger expected = 0;
+    NSMutableString *numbersToAdd = [NSMutableString string];
+    for (NSInteger i = 0; i < randomAmount; i++) {
+        [numbersToAdd appendString:[NSString stringWithFormat:@"%i,",i]];
+        expected += i;
+    }
+
+    NSInteger result = [sut add:numbersToAdd];
+
+    STAssertEquals(expected, result, @"Two length input should return sum.");
+}
+
 
 @end
