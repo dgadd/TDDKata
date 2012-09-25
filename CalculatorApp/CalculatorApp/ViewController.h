5afbb276148d9a24f85b89a8fb8cb0015b9fdf55
David Gadd <gaddzeit@yahoo.ca>
2012-09-25 07:57:55 -0700

    Test 1: Zero length input should return 0

---
+++ CalculatorApp/CalculatorApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CalculatorApp
+//
+//  Created by David Gadd on 09/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
