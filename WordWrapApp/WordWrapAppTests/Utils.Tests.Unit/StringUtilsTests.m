72cc0ce1c69ce89038e81705fd15949377aa5f48
David Gadd <gaddzeit@yahoo.ca>
2012-09-26 09:14:39 -0700

    Test 2: When column width is 20, line should return with exact breaks

--- WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
+++ WordWrapApp/WordWrapAppTests/Utils.Tests.Unit/StringUtilsTests.m
@@ -16,5 +16,13 @@
     STAssertEqualObjects(expected, result, @"When column width is 0, line should return with no breaks.");
 }
 
+- (void)testWrapLineByColumnWidthMethod_columnWidth20_returnsLineWithExactBreaks {
+    NSString *input = @"Here is a line with multi-hypenated-context and 3.145 decimal numbers.";
+    NSString *expected = @"Here is a line with \nmulti-hypenated-cont\next and 3.145 decima\nl numbers.";
+
+    NSString *result = [sut wrapLine:input byColumnWidth:20];
+    STAssertEqualObjects(expected, result, @"When column width is 20, line should return with exact breaks.");
+}
+
 
 @end
