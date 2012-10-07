9be286d72947a313466775ce202741c9d8685a73
David Gadd <gaddzeit@yahoo.ca>
2012-10-07 10:48:30 -0700

    Test 1: Zero length input should return 0

---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorAppTests.h
+//  CalculatorAppTests
+//
+//  Created by David Gadd on 10/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CalculatorAppTests : SenTestCase
+
+@end
---
+++ CalculatorApp/CalculatorAppTests/CalculatorAppTests.m
@@ -0,0 +1,32 @@
+//
+//  CalculatorAppTests.m
+//  CalculatorAppTests
+//
+//  Created by David Gadd on 10/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorAppTests.h"
+
+@implementation CalculatorAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CalculatorAppTests");
+}
+
+@end
