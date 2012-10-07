955098cd7b96661e2d5d7ecf64b022f3021a7097
David Gadd <gaddzeit@yahoo.ca>
2012-10-07 11:10:12 -0700

    Test 11: Custom multi-length multi-delimiter input should return sum

--- CalculatorApp/CalculatorAppTests/Domain.Tests.Unit/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/Domain.Tests.Unit/CalculatorTests.m
@@ -84,5 +84,12 @@
     STAssertEquals(expected, result, @"Custom multi-length delimiter input should return sum.");
 }
 
+- (void)testAddMethod_customMultiLengthMultiDelimiterInput_shouldReturnSum {
+    NSInteger expected = 17;
+    NSInteger result = [sut add:@"//[######][@@@]\n3######5@@@2,4\n3"];
+
+    STAssertEquals(expected, result, @"Custom multi-length multi-delimiter input should return sum.");
+}
+
 
 @end
