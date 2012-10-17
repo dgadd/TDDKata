29dcc93b98d44e5c4985cf410bff5be8c983f571
David Gadd <gaddzeit@yahoo.ca>
2012-10-17 07:23:07 -0700

    Test 1: Mock test of ISanityCheckRepository protocol is verified

---
+++ MvpDemoApp/MvpDemoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MvpDemoApp
+//
+//  Created by David Gadd on 10/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
