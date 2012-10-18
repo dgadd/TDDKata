f021f6b3f0dfecb0df975b1b3c5bc84a4c8651a2
David Gadd <gaddzeit@yahoo.ca>
2012-10-18 07:04:13 -0700

    Test 1: SanityCheck test verifies the protocol ISanityCheckRepository

---
+++ MvpAppDemo/MvpAppDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MvpAppDemo
+//
+//  Created by David Gadd on 10/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MvpAppDemo/MvpAppDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MvpAppDemo
+//
+//  Created by David Gadd on 10/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
