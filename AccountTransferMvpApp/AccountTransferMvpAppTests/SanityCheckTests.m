810dedc9322cdef4372705de4704cf2425c01442
David Gadd <gaddzeit@yahoo.ca>
2012-10-20 12:22:02 -0700

    Test 1: SanityCheck test with OCMock verifies expectations

---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/SanityCheckTests.m
@@ -0,0 +1,13 @@
+#import "SanityCheckTests.h"
+#import "OCMockObject.h"
+#import "ISanityCheckRepository.h"
+
+@implementation SanityCheckTests
+
+- (void)testSanityCheck {
+    id sanityCheckRepository = [OCMockObject mockForProtocol:@protocol(ISanityCheckRepository)];
+
+    [sanityCheckRepository verify];
+}
+
+@end
