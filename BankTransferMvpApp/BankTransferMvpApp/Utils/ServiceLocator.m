dce71876711cb21fc6d6636a7c22a912304d1f38
David Gadd <gaddzeit@yahoo.ca>
2012-10-21 12:26:19 -0700

    Test 7: When ServiceLocator register is passed AppLaunch enum value, then the corresponding presenter is returned

--- BankTransferMvpApp/BankTransferMvpApp/Utils/ServiceLocator.m
+++ BankTransferMvpApp/BankTransferMvpApp/Utils/ServiceLocator.m
@@ -3,6 +3,9 @@
 #import "PresenterTypeEnum.h"
 #import "LocalAccountRepository.h"
 #import "RemoteAccountRepository.h"
+#import "AppLaunchPresenter.h"
+#import "AdManagerLocationRepository.h"
+#import "MapLocationRepository.h"
 
 
 @implementation ServiceLocator {
@@ -10,16 +13,27 @@
 }
 
 + (id)register:(PresenterTypeEnum)presenterType {
-    id<IRemoteAccountRepository> remoteAccountRepository = [[RemoteAccountRepository alloc] init];
-    id<ILocalAccountRepository> localAccountRepository = [[LocalAccountRepository alloc] init];
-
     switch (presenterType) {
         case AppLaunch:
-            return nil;
+            return [self generateAppLaunchPresenter];
         case AccountTransfer:
-            return [[AccountTransferPresenter alloc] initWithRemote:remoteAccountRepository
-                                                    andLocal:localAccountRepository];
+            return [self generateAccountTransferPresenter];
     }
     return nil;
 }
+
++ (AccountTransferPresenter *)generateAccountTransferPresenter {
+    id<IRemoteAccountRepository> remoteAccountRepository = [[RemoteAccountRepository alloc] init];
+    id<ILocalAccountRepository> localAccountRepository = [[LocalAccountRepository alloc] init];
+    return [[AccountTransferPresenter alloc] initWithRemote:remoteAccountRepository
+                                                                                                 andLocal:localAccountRepository];
+}
+
++ (AppLaunchPresenter *)generateAppLaunchPresenter {
+    id<IMapLocationRepository> mapLocationRepository = [[MapLocationRepository alloc] init];
+    id<IAdManagerRepository> adManagerRepository = [[AdManagerLocationRepository alloc] init];
+
+    return [[AppLaunchPresenter alloc] initWithMap:mapLocationRepository andAdManager:adManagerRepository];
+}
+
 @end
--- BankTransferMvpApp/BankTransferMvpAppTests/Utils.Tests.Unit/ServiceLocatorTests.m
+++ BankTransferMvpApp/BankTransferMvpAppTests/Utils.Tests.Unit/ServiceLocatorTests.m
@@ -2,14 +2,22 @@
 #import "PresenterTypeEnum.h"
 #import "ServiceLocator.h"
 #import "AccountTransferPresenter.h"
+#import "AppLaunchPresenter.h"
 
 @implementation ServiceLocatorTests
 
--(void)test_whenRegisterIsPassedEnumValue_thenMatchingPresenterIsReturned {
+-(void)test_whenRegisterIsPassedAccountTransferEnumValue_thenMatchingPresenterIsReturned {
     PresenterTypeEnum presenterTypeEnum = AccountTransfer;
     id accountTransferPresenter = [ServiceLocator register:presenterTypeEnum];
 
     STAssertTrue([accountTransferPresenter isKindOfClass:AccountTransferPresenter.class], @"The correct presenter should have been returned.");
+}
+
+-(void)test_whenRegisterIsPassedAppLaunchEnumValue_thenMatchingPresenterIsReturned {
+    PresenterTypeEnum presenterTypeEnum = AppLaunch;
+    id appLaunchPresenter = [ServiceLocator register:presenterTypeEnum];
+
+    STAssertTrue([appLaunchPresenter isKindOfClass:AppLaunchPresenter.class], @"The correct presenter should have been returned.");
 }
 
 @end
