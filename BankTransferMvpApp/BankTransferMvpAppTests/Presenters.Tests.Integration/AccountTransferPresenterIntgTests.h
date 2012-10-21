e798b576127d99a1a0d9f58e5002ecc7719d9d8f
David Gadd <gaddzeit@yahoo.ca>
2012-10-21 10:18:36 -0700

    Test 3: Integration test verifies that when the presenter calls transferBetweenAccounts, then the delegated repositories perform the transfer

---
+++ BankTransferMvpApp/BankTransferMvpAppTests/Presenters.Tests.Integration/AccountTransferPresenterIntgTests.h
@@ -0,0 +1,5 @@
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AccountTransferPresenterIntgTests : SenTestCase
+
+@end
---
+++ BankTransferMvpApp/BankTransferMvpAppTests/Presenters.Tests.Integration/AccountTransferPresenterIntgTests.m
@@ -0,0 +1,29 @@
+#import "AccountTransferPresenterIntgTests.h"
+#import "IAccountTransferView.h"
+#import "IRemoteAccountRepository.h"
+#import "RemoteAccountRepository.h"
+#import "ILocalAccountRepository.h"
+#import "LocalAccountRepository.h"
+#import "AccountTransferPresenter.h"
+#import "OCMockObject.h"
+#import "OCMockRecorder.h"
+
+@implementation AccountTransferPresenterIntgTests
+
+-(void)test_whenTransferBetweenAccountsMethodIsCalled_thenAllDelegatedRepositoryMethodsPerformTheTransfer {
+    NSNumber *amount = [NSNumber numberWithDouble:150.00];
+
+    id accountTransferViewStub = [OCMockObject mockForProtocol:@protocol(IAccountTransferView)];
+    [[[accountTransferViewStub stub] andReturn:amount] getTransferAmount];
+    id<IRemoteAccountRepository> remoteAccountRepository = [[RemoteAccountRepository alloc] init];
+    id<ILocalAccountRepository> localAccountRepository = [[LocalAccountRepository alloc] init];
+    [[accountTransferViewStub expect] setResultMessage:@"The transfer request of $150 succeeded."];
+
+    AccountTransferPresenter *sut = [[AccountTransferPresenter alloc] initWithView:accountTransferViewStub
+                                                                         andRemote:remoteAccountRepository
+                                                                          andLocal:localAccountRepository];
+    [sut transferBetweenAccounts];
+    [accountTransferViewStub verify];
+}
+
+@end
