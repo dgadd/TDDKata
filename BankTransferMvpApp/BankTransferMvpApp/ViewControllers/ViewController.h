b18584ef4a03737e28ade49f3b4e917f71c95539
David Gadd <gaddzeit@yahoo.ca>
2012-10-21 09:42:59 -0700

    Test 1: SanityCheck test verifies that mocked sanitycheck repository is called

---
+++ BankTransferMvpApp/BankTransferMvpApp/ViewControllers/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BankTransferMvpApp
+//
+//  Created by David Gadd on 10/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ BankTransferMvpApp/BankTransferMvpAppTests/SanityCheckTests.h
@@ -0,0 +1,13 @@
+//
+//  SanityCheckTests.h
+//  SanityCheckTests
+//
+//  Created by David Gadd on 10/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SanityCheckTests : SenTestCase
+
+@end
