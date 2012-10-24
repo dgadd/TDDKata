10798b9e0cb7e3a098eb37808e92986e1b25e9d2
David Gadd <gaddzeit@yahoo.ca>
2012-10-24 07:15:25 -0700

    Test 2: When AccountTransferPresenter transferAmount called, then delegated repository methods are called

---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/Presenters.Tests.Unit/AccountTransferPresenterTests.m
@@ -0,0 +1,34 @@
+#import "AccountTransferPresenterTests.h"
+#import "OCMockObject.h"
+#import "IAccountTransferView.h"
+#import "IRemoteAccountRepository.h"
+#import "ILocalAccountRepository.h"
+#import "OCMockRecorder.h"
+#import "AccountTransferPresenter.h"
+
+@implementation AccountTransferPresenterTests
+
+-(void)test_whenTransferAmountMethodIsCalled_thenDelegatedRepositoryMethodsAreCalledInOrder {
+    id accountTransferView = [OCMockObject mockForProtocol:@protocol(IAccountTransferView)];
+    id remoteAccountRepository = [OCMockObject mockForProtocol:@protocol(IRemoteAccountRepository)];
+    id localAccountRepository = [OCMockObject mockForProtocol:@protocol(ILocalAccountRepository)];
+    
+    // set expectations
+    NSNumber *amount = [NSNumber numberWithDouble:150.0];
+    [[[accountTransferView expect] andReturn:amount] getTransferAmount];
+    [[remoteAccountRepository expect] withdrawAmount:amount];
+    [[localAccountRepository expect] depositAmount:amount];
+    [[accountTransferView expect] setMessage:@"Transfer of $150 completed."];
+
+    AccountTransferPresenter *sut = [[AccountTransferPresenter alloc] init];
+    sut.accountTransferView = (id<IAccountTransferView>)accountTransferView;
+    sut.remoteAccountRepository = (id<IRemoteAccountRepository>)remoteAccountRepository;
+    sut.localAccountRepository = (id<ILocalAccountRepository>)localAccountRepository;
+    [sut transferAmount];
+
+    [accountTransferView verify];
+    [remoteAccountRepository verify];
+    [localAccountRepository verify];
+}
+
+@end
