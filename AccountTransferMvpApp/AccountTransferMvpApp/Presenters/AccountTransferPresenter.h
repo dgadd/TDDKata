15b1965686e66c91104ad100dd7f8eee3a91c145
David Gadd <gaddzeit@yahoo.ca>
2012-10-26 08:34:55 -0700

    Refactored protocol assignments from properties to init overload

--- AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.h
+++ AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.h
@@ -7,8 +7,10 @@
 
 @interface AccountTransferPresenter : NSObject
 @property(nonatomic, strong) id <IAccountTransferView> accountTransferView;
-@property(nonatomic, strong) id <IRemoteAccountRepository> remoteAccountRepository;
-@property(nonatomic, strong) id <ILocalAccountRepository> localAccountRepository;
+@property(nonatomic, strong, readonly) id <IRemoteAccountRepository> remoteAccountRepository;
+@property(nonatomic, strong, readonly) id <ILocalAccountRepository> localAccountRepository;
 
 - (void)transferAmount;
+
+- (id)initWithRemote:(id <IRemoteAccountRepository>)remoteAccountRepository andLocal:(id <ILocalAccountRepository>)localAccountRepository;
 @end
--- AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.m
+++ AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.m
@@ -16,6 +16,14 @@
     NSNumber *amount = [_accountTransferView getTransferAmount];
     [_remoteAccountRepository withdrawAmount:amount];
     [_localAccountRepository depositAmount:amount];
-    [_accountTransferView displayMessage:[NSString stringWithFormat:@"$%@ was transferred",amount]];
+    [_accountTransferView displayMessage:[NSString stringWithFormat:@"$%@ was transferred", amount]];
+}
+
+- (id)initWithRemote:(id <IRemoteAccountRepository>)remoteAccountRepository andLocal:(id <ILocalAccountRepository>)localAccountRepository {
+    if (self = [super init]) {
+        _remoteAccountRepository = remoteAccountRepository;
+        _localAccountRepository = localAccountRepository;
+    }
+    return self;
 }
 @end
