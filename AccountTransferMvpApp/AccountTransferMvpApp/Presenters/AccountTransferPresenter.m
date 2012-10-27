87e317c7584426779bf2097c5e759da37eb1cc5b
David Gadd <gaddzeit@yahoo.ca>
2012-10-27 09:24:45 -0700

    Test 2: When AccountTransferPresenter transferAmount called, then expected delegated repository methods are called

---
+++ AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.m
@@ -0,0 +1,18 @@
+#import "AccountTransferPresenter.h"
+
+
+@implementation AccountTransferPresenter {
+
+}
+@synthesize accountTransferView = _accountTransferView;
+@synthesize remoteAccountRepository = _remoteAccountRepository;
+@synthesize localAccountRepository = _localAccountRepository;
+
+
+- (void)transferAmount {
+    NSNumber *amount = [_accountTransferView getTransferAmount];
+    [_remoteAccountRepository withdrawAmount:amount];
+    [_localAccountRepository depositAmount:amount];
+    [_accountTransferView displayMessage:[NSString stringWithFormat:@"$%@ transferred.",amount]];
+}
+@end
