3859eb4ba1f418ed2a77d092a5d3c7f41fc61c62
David Gadd <gaddzeit@yahoo.ca>
2012-10-28 12:41:40 -0700

    Implemented ServiceLocator and the Presenter in the actiivty and displayed result in emulator

--- AccountTransferMvpApp/src/com/gaddzeit/AccountTransferActivity.java
+++ AccountTransferMvpApp/src/com/gaddzeit/AccountTransferActivity.java
@@ -2,8 +2,13 @@ package com.gaddzeit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.gaddzeit.presenters.AccountTransferPresenter;
+import com.gaddzeit.utils.PresenterTypeEnum;
+import com.gaddzeit.utils.ServiceLocator;
+import com.gaddzeit.views.IAccountTransferView;
 
-public class AccountTransferActivity extends Activity {
+public class AccountTransferActivity extends Activity implements IAccountTransferView {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,20 @@ public class AccountTransferActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final AccountTransferPresenter accountTransferPresenter = (AccountTransferPresenter) ServiceLocator.resolve(PresenterTypeEnum.AccountTransferPresenter);
+        accountTransferPresenter.setAccountTransferView(this);
+        accountTransferPresenter.transferAmount();
+    }
+
+    @Override
+    public double getTransferAmount() {
+        return 150.0;
+    }
+
+    @Override
+    public void setDisplayMessage(String message) {
+        TextView textDisplayMessage = (TextView) findViewById(R.id.displayMessage);
+        textDisplayMessage.setText(message);
     }
 }
