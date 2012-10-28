f6f00c58548e9e4e902c87bec2d24665c420f3f6
David Gadd <gaddzeit@yahoo.ca>
2012-10-28 09:19:22 -0700

    Test 1: SanityCheck test verifies that OCMock is installed and verifying a protocol

---
+++ AccountTransferMvpAp/AccountTransferMvpAp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AccountTransferMvpAp
+//
+//  Created by David Gadd on 10/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ AccountTransferMvpAp/AccountTransferMvpAp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AccountTransferMvpAp
+//
+//  Created by David Gadd on 10/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
