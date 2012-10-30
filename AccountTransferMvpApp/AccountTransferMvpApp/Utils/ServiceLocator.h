6b4b3a5a7eee61f774c17ddc20e0b0c1c70dbe99
David Gadd <gaddzeit@yahoo.ca>
2012-10-29 21:03:36 -0700

    Test 4: When ServiceLocator resolve is passed an enum, the matching presenter is returned

---
+++ AccountTransferMvpApp/AccountTransferMvpApp/Utils/ServiceLocator.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+#import "PresenterTypeEnum.h"
+
+@interface ServiceLocator : NSObject
++ (id)resolve:(PresenterTypeEnum)presenterTypeEnum;
+@end
---
+++ AccountTransferMvpApp/AccountTransferMvpApp/Utils/ServiceLocator.m
@@ -0,0 +1,28 @@
+#import "ServiceLocator.h"
+#import "PresenterTypeEnum.h"
+#import "AccountTransferPresenter.h"
+#import "LocalAccountRepository.h"
+#import "RemoteAccountRepository.h"
+
+
+@implementation ServiceLocator {
+
+}
+
++ (id)resolve:(PresenterTypeEnum)presenterTypeEnum {
+    switch (presenterTypeEnum) {
+        case AppLaunch:
+            return nil;
+        case AccountTransfer:
+            return [self createAccountTransferPresenter];
+    }
+    return nil;
+}
+
++ (id)createAccountTransferPresenter {
+    id<IRemoteAccountRepository> remoteAccountRepository = [[RemoteAccountRepository alloc] init];
+    id<ILocalAccountRepository> localAccountRepository = [[LocalAccountRepository alloc] init];
+    AccountTransferPresenter *accountTransferPresenter = [[AccountTransferPresenter alloc] initWithRemote:remoteAccountRepository andLocal:localAccountRepository];
+    return accountTransferPresenter;
+}
+@end
