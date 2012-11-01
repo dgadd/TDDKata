0811de0b673be0e92626800c12afc1251586bb65
David Gadd <gaddzeit@yahoo.ca>
2012-11-01 08:00:41 -0700

    Test 2: When AccountTransferPresenter transferAmount is requested, all delegated protocol methods are called

---
+++ AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.h
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+@protocol IRemoteAccountRepository;
+@protocol ILocalAccountRepository;
+@protocol IAccountTransferView;
+
+
+@interface AccountTransferPresenter : NSObject
+@property(nonatomic, strong, readonly) id <IRemoteAccountRepository> remoteAccountRepository;
+@property(nonatomic, strong, readonly) id <ILocalAccountRepository> localAccountRepository;
+
+@property(nonatomic, strong) id <IAccountTransferView> accountTransferView;
+
+- (id)initWithRemote:(id <IRemoteAccountRepository>)remoteAccountRepository andLocal:(id <ILocalAccountRepository>)localAccountRepository;
+
+- (void)transferAmount;
+@end
---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/Presenters.Tests.Unit/AccountTransferPresenterTests.m
@@ -0,0 +1,32 @@
+#import "AccountTransferPresenterTests.h"
+#import "OCMockObject.h"
+#import "IAccountTransferView.h"
+#import "IRemoteAccountRepository.h"
+#import "ILocalAccountRepository.h"
+#import "OCMockRecorder.h"
+#import "AccountTransferPresenter.h"
+
+@implementation AccountTransferPresenterTests
+
+-(void)test_whenTransferAmountRequested_thenAllDelegatedProtocolsAreCalled {
+    id accountTransferView = [OCMockObject mockForProtocol:@protocol(IAccountTransferView)];
+    id remoteAccountRepository = [OCMockObject mockForProtocol:@protocol(IRemoteAccountRepository)];
+    id localAccountRepository = [OCMockObject mockForProtocol:@protocol(ILocalAccountRepository)];
+
+    NSNumber *amount = [NSNumber numberWithDouble:150.0];
+    [[[accountTransferView expect] andReturn:amount] getTransferAmount];
+    [[remoteAccountRepository expect] withdrawAmount:amount];
+    [[localAccountRepository expect] depositAmount:amount];
+    [[accountTransferView expect] setDisplayMessage:@"$150 transferred."];
+
+    AccountTransferPresenter *sut = [[AccountTransferPresenter alloc] initWithRemote:remoteAccountRepository
+                                                                            andLocal:localAccountRepository];
+    sut.accountTransferView = (id<IAccountTransferView>)accountTransferView;
+    [sut transferAmount];
+    
+    [accountTransferView verify];
+    [remoteAccountRepository verify];
+    [localAccountRepository verify];
+}
+
+@end
