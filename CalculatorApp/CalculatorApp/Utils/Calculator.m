7265e4cb73fc65fa2e317d1843653623076e2f82
David Gadd <gaddzeit@yahoo.ca>
2012-11-05 20:19:35 -0800

    Test 3: Two length input should return sum

--- CalculatorApp/CalculatorApp/Utils/Calculator.m
+++ CalculatorApp/CalculatorApp/Utils/Calculator.m
@@ -6,6 +6,18 @@
 }
 
 - (NSInteger)add:(NSString *)numbersToAdd {
+    if ([numbersToAdd rangeOfString:@","].location != NSNotFound)
+        return [self sum:numbersToAdd];
+
     return [numbersToAdd length] > 0 ? [numbersToAdd integerValue] : 0;
+}
+
+- (NSInteger)sum:(NSString *)numbersToAdd {
+    NSArray *numbersArray = [numbersToAdd componentsSeparatedByString:@","];
+    NSInteger total = 0;
+    for (NSString *numberString in numbersArray) {
+        total += [numberString integerValue];
+    }
+    return total;
 }
 @end
