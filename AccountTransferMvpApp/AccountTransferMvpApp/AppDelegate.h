028464976e9c46fa3fa19aadcdc42a9ca87b88ff
David Gadd <gaddzeit@yahoo.ca>
2012-11-05 20:49:56 -0800

    Test 1: Sanity check test verifies that OCMock is installed and can verify mocked protocol

---
+++ AccountTransferMvpApp/AccountTransferMvpApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AccountTransferMvpApp
+//
+//  Created by David Gadd on 11/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
