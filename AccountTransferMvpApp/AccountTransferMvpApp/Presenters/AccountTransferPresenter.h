d9df214530c505a326b87bc0e215a83cec8e2593
David Gadd <gaddzeit@yahoo.ca>
2012-11-08 22:44:47 -0800

    Test 2: When AccountTransferPresenter transferAmount is requested, then all delegated protocols are called

---
+++ AccountTransferMvpApp/AccountTransferMvpApp/Presenters/AccountTransferPresenter.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+#import "IRemoteAccountRepository.h"
+#import "ILocalAccountRepository.h"
+#import "IAccountTransferView.h"
+
+@interface AccountTransferPresenter : NSObject
+@property(nonatomic, strong) id <IRemoteAccountRepository> remoteAccountRepository;
+
+@property(nonatomic, strong) id <ILocalAccountRepository> localAccountRepository;
+
+@property(nonatomic, strong) id <IAccountTransferView> accountTransferView;
+
+- (id)initWithRemote:(id <IRemoteAccountRepository>)remoteAccountRepository andLocal:(id <ILocalAccountRepository>)localAccountRepository;
+
+- (void)transferAmount;
+@end
