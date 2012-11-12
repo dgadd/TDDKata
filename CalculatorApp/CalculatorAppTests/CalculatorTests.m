679980e49e29278502586af4f6220bde2b133680
David Gadd <gaddzeit@yahoo.ca>
2012-11-12 10:46:43 -0800

    Test 2: One length input should return numeric equivalent

--- CalculatorApp/CalculatorAppTests/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/CalculatorTests.m
@@ -15,4 +15,13 @@
     STAssertEquals(expected, result, @"Zero length input should return 0.");
 }
 
+- (void)testGivenSut_whenOneLengthInput_thenNumericEquivalentShouldBeReturned {
+    NSInteger expected = 9;
+    NSInteger result = [sut add:@"9"];
+
+    STAssertEquals(expected, result, @"One length input should return numeric equivalent.");
+}
+
+
+
 @end
