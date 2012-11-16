c0618b8abe839c3f4f6e0ad799186bc90b981b7c
David Gadd <gaddzeit@yahoo.ca>
2012-11-15 18:55:03 -0800

    Test 1: SanityCheck tests verify OCMock installed correctly

---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/SanityCheckTests.m
@@ -0,0 +1,13 @@
+#import "SanityCheckTests.h"
+#import "OCMockObject.h"
+#import "ISanityCheck.h"
+
+@implementation SanityCheckTests
+
+- (void)testSanityCheck {
+    id sanityCheck = [OCMockObject mockForProtocol:@protocol(ISanityCheck)];
+
+    [sanityCheck verify];
+}
+
+@end
