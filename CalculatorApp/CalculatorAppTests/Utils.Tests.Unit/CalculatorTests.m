1d77abb95948c9542427108601194ba0c7b1ae5a
David Gadd <gaddzeit@yahoo.ca>
2012-11-19 19:12:19 -0800

    Test 1: Given Add method, when one length string is input, then numeric equivalent should be returned.

--- CalculatorApp/CalculatorAppTests/Utils.Tests.Unit/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/Utils.Tests.Unit/CalculatorTests.m
@@ -17,5 +17,12 @@
     STAssertEquals(expected, result, @"When zero length string is input, then 0 should be returned.");
 }
 
+-(void)testGivenAddMethod_whenInputOneLengthString_thenNumericEquivalentIsReturned {
+    NSInteger expected = 9;
+    NSInteger result = [_sut add:@"9"];
+
+    STAssertEquals(expected, result, @"When one length string is input, then numeric equivalent should be returned.");
+}
+
 
 @end
