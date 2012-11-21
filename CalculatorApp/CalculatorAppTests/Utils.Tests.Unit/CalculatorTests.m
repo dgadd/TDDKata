9f33f23a2b6ef3d8f3d62fd2ac8065c9f009258c
David Gadd <gaddzeit@yahoo.ca>
2012-11-21 06:45:26 -0800

    Test 1: When zero length string is input, then numeric 0 should be returned

---
+++ CalculatorApp/CalculatorAppTests/Utils.Tests.Unit/CalculatorTests.m
@@ -0,0 +1,21 @@
+#import "CalculatorTests.h"
+#import "Calculator.h"
+
+@implementation CalculatorTests {
+    Calculator *_sut;
+}
+
+- (void)setUp {
+    [super setUp];
+    _sut = [[Calculator alloc] init];
+}
+
+-(void)testGivenAddMethod_whenAZeroLengthStringIsInput_thenNumericZeroIsReturned {
+    NSInteger expected = 0;
+    NSInteger result = [_sut add:@""];
+
+    STAssertEquals(expected, result, @"When a zero length string is input, then numeric zero should be returned.");
+}
+
+
+@end
