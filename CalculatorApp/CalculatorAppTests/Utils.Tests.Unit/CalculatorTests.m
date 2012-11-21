a46094728321a834425bfb4ac6ebb5b14ad0e6f8
David Gadd <gaddzeit@yahoo.ca>
2012-11-21 06:51:41 -0800

    Test 4: When any length comma-delimited string is input, then sum should be returned

--- CalculatorApp/CalculatorAppTests/Utils.Tests.Unit/CalculatorTests.m
+++ CalculatorApp/CalculatorAppTests/Utils.Tests.Unit/CalculatorTests.m
@@ -24,9 +24,22 @@
     STAssertEquals(expected, result, @"When a one length string is input, then numeric equivalent should be returned.");
 }
 
--(void)testGivenAddMethod_whenACommaDelimitedLengthStringIsInput_thenSumIsReturned {
+-(void)testGivenAddMethod_whenATwoLengthCommaDelimitedStringIsInput_thenSumIsReturned {
     NSInteger expected = 9;
     NSInteger result = [_sut add:@"4,5"];
+
+    STAssertEquals(expected, result, @"When a two length comma-delimited string is input, then sum should be returned.");
+}
+
+-(void)testGivenAddMethod_whenAnyLengthCommaDelimitedStringIsInput_thenSumIsReturned {
+    NSInteger expected = 0;
+    NSInteger randomAmount = arc4random() % 999;
+    NSMutableString *numbersToAdd = [NSMutableString string];
+    for (NSInteger i = 0; i < randomAmount; i++) {
+        [numbersToAdd appendString:[NSString stringWithFormat:@"%i,",i]];
+        expected += i;
+    }
+    NSInteger result = [_sut add:numbersToAdd];
 
     STAssertEquals(expected, result, @"When a two length comma-delimited string is input, then sum should be returned.");
 }
