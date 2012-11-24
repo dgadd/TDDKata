627ac4351268d08ecbb5142d74f2db1d06cb1a8d
David Gadd <gaddzeit@yahoo.ca>
2012-11-24 10:09:39 -0800

    Test 2: When AccountTransferPresenter tranfser amount is called, then all delegated protocols are called

---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/Presenters.Tests.Unit/AccountTransferPresenterTests.h
@@ -0,0 +1,13 @@
+//
+//  AccountTransferPresenterTests.h
+//  AccountTransferMvpApp
+//
+//  Created by David Gadd on 11/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AccountTransferPresenterTests : SenTestCase
+
+@end
