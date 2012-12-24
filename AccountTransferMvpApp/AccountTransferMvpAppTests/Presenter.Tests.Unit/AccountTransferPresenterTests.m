9f1c68f9c1ee90287947f45f5c18e37465ac05a8
David Gadd <gaddzeit@yahoo.ca>
2012-12-23 17:48:42 -0800

    Test 1: When AccountTransferPresenter transferAmount is requested, then all delegated protocols are called

---
+++ AccountTransferMvpApp/AccountTransferMvpAppTests/Presenter.Tests.Unit/AccountTransferPresenterTests.m
@@ -0,0 +1,34 @@
+#import "AccountTransferPresenterTests.h"
+#import "OCMockObject.h"
+#import "IAccountTransferView.h"
+#import "IRemoteAccountRepository.h"
+#import "ILocalAccountRepository.h"
+#import "OCMockRecorder.h"
+#import "AccountTransferPresenter.h"
+
+@implementation AccountTransferPresenterTests
+
+-(void)testGivenSut_whenTransferAmountRequested_thenAllDelegatedProtocolsAreCalled {
+    id accountTransferView = [OCMockObject mockForProtocol:@protocol(IAccountTransferView)];
+    id remoteAccountRepository = [OCMockObject mockForProtocol:@protocol(IRemoteAccountRepository)];
+    id localAccountRepository = [OCMockObject mockForProtocol:@protocol(ILocalAccountRepository)];
+
+    // set expectations
+    NSNumber *amount = @150.0;
+    [[[accountTransferView expect] andReturn:amount] getTransferAmount];
+    [[remoteAccountRepository expect] withdrawAmount:amount];
+    [[localAccountRepository expect] depositAmount:amount];
+    [[accountTransferView expect] setDisplayMessage:@"$150 transferred."];
+
+    // instantiate sut
+    AccountTransferPresenter *sut = [[AccountTransferPresenter alloc] initWithRemote:remoteAccountRepository andLocal:localAccountRepository];
+    sut.accountTransferView = (id<IAccountTransferView>)accountTransferView;
+    [sut transferAmount];
+
+    // verify
+    [accountTransferView verify];
+    [remoteAccountRepository verify];
+    [localAccountRepository verify];
+}
+
+@end
