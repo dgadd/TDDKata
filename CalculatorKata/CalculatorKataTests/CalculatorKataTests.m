cc87125e961ad372985b0b2accb7555222b40323
David Gadd <gaddzeit@yahoo.ca>
2013-11-23 09:54:36 -0800

    KATA-1 Given any length comma-separated number string, the sum of numbers is returned

--- CalculatorKata/CalculatorKataTests/CalculatorKataTests.m
+++ CalculatorKata/CalculatorKataTests/CalculatorKataTests.m
@@ -5,28 +5,48 @@
 
 @end
 
-@implementation CalculatorKataTests
+@implementation CalculatorKataTests {
+    Calculator *_sut;
+}
+
+- (void)setUp {
+    [super setUp];
+    _sut = [[Calculator alloc] init];
+}
+
 
 -(void)test_givenSut_whenZeroLengthStringInput_thenZeroReturned {
     NSInteger expected = 0;
-    Calculator *sut = [[Calculator alloc] init];
-    NSInteger result = [sut add:@""];
+    NSInteger result = [_sut add:@""];
 
     XCTAssertEqual(expected, result);
 }
 
 -(void)test_givenSut_whenOneLengthStringInput_thenNumericEquivalentReturned {
     NSInteger expected = 3;
-    Calculator *sut = [[Calculator alloc] init];
-    NSInteger result = [sut add:@"3"];
+    NSInteger result = [_sut add:@"3"];
 
     XCTAssertEqual(expected, result);
 }
 
 -(void)test_givenSut_whenTwoLengthStringInput_thenNumericEquivalentReturned {
     NSInteger expected = 9;
-    Calculator *sut = [[Calculator alloc] init];
-    NSInteger result = [sut add:@"3,6"];
+    NSInteger result = [_sut add:@"3,6"];
+
+    XCTAssertEqual(expected, result);
+}
+
+-(void)test_givenSut_whenAnyLengthStringInput_thenNumericEquivalentReturned {
+    NSInteger howMany = arc4random() % 9999;
+    NSInteger expected = 0;
+
+    NSMutableString *randomNumberString = [NSMutableString string];
+    for(int i = 0; i < howMany; i++) {
+        [randomNumberString appendFormat:@"%d,", i];
+        expected += i;
+    }
+
+    NSInteger result = [_sut add:randomNumberString];
 
     XCTAssertEqual(expected, result);
 }
